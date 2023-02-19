e52f6e79f1ee8e251aa46b4c4cf5f446b24d8991
behzad azadi <48598137+behzad-azadi2693@users.noreply.github.com>
2023-02-19 23:20:23 +0330

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -120,8 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-#STATIC_ROOT = '/home/nisanbar/public_html/static'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = '/home/arghavan/public_html/static'
 
 
 STATICFILES_DIRS = [
@@ -129,8 +129,8 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-#MEDIA_ROOT = '/home/nisanbar/public_html/media'
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/home/arghavan/public_html/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
